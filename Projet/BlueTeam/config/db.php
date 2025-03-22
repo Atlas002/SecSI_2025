c134333c029db70c32c1cccc49883825e0976df8
Poulereaper <mathis.grs05@gmail.com>
2025-03-22 18:09:47 +0100

    fix : erreur dans db.php

--- Projet/BlueTeam/config/db.php
+++ Projet/BlueTeam/config/db.php
@@ -2,7 +2,7 @@
 $host = 'db'; // Le nom du service MySQL dans docker-compose
 $dbname = 'ece_db';
 $username = 'user';
-$password = 'password';
+$password = 'nv7_4f8X.g1qPPP';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
